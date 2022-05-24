3bb2eb28c6a7aba020412f1439a44cd5627a46a4
lomantic <77719867+lomantic@users.noreply.github.com>
2022-05-24 16:27:45 +0900

    setup 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { HashRouter as Router, Route } from "react-router-dom";
+import { useReactiveVar } from "@apollo/client";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { isLoggedInVar } from "./apollo";
+import Home from "./screen/Home";
+import Login from "./screen/Login";
+import NotFound from "./screen/NotFound";
 
 function App() {
-  return <div></div>;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
